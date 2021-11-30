8d805c12d3ab3ad02e773f55984ca9f127269e95
Lokalni CroRIS4 <lu-croris4@srce.hr>
2021-11-30 19:31:30 +0100

    hotfix: add boot.sh

---
+++ docs/boot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ng new workspace-ngx-rapid-interface-builder --no-create-application
+cd workspace-ngx-rapid-interface-builder
+rm -rf .git
+ng generate library ngx-rapid-interface-builder
+ng generate application ngx-rapid-interface-builder-test --style=scss --routing=false
+npm install faker && npm install --save-dev @types/faker && npm install --save-dev @types/lodash
+cd projects/ngx-rapid-interface-builder
+rm -rfv * && rm -rfv .*
+git clone https://github.com/brunotot/ngx-rapid-interface-builder.git .
+npm install
+npm run lib:build
+unzip -o ./docs/app.zip -d ./../ngx-rapid-interface-builder-test/src
+code ../../.
+
