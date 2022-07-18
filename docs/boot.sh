1c2097b89b4b16e336df453b353932b5c64a1562
crorisvanjski4 <crorisvanjski4@crorisvanjski4>
2022-07-18 18:04:47 +0200

    fix readme and boot sh

--- docs/boot.sh
+++ docs/boot.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
 # Create workspace
-ng new workspace-ngx-rapid-interface-builder --no-create-application
+ng new workspace-ngx-input-annotations --no-create-application
 
 # Change directory to created workspace
-cd workspace-ngx-rapid-interface-builder
+cd workspace-ngx-input-annotations
 
 # Remove unnecessary git init folder
 rm -rf .git
 
 # Generate library for development
-ng generate library ngx-rapid-interface-builder
+ng generate library ngx-input-annotations
 
 # Generate testing application that will consume the library
-ng generate application ngx-rapid-interface-builder-test --style=scss --routing=false
+ng generate application ngx-input-annotations-demo --style=scss --routing=false
 
 # Install prepared dependencies for testing application
-npm install faker && npm install --save-dev @types/faker && npm install --save-dev @types/lodash
+npm i @faker-js/faker reflect-metadata language-flag-colors
+npm i @angular/material@13.3.0 @angular/cdk@13.3.0 @angular/animations@13.3.0 --save --force
 
 # Change directory to created library
-cd projects/ngx-rapid-interface-builder
+cd projects/ngx-input-annotations
 
 # Remove all files from the auto-generated library
 rm -rfv * && rm -rfv .*
 
 # Clone the repository
-git clone https://github.com/brunotot/ngx-rapid-interface-builder.git .
+git clone https://github.com/brunotot/ngx-input-annotations.git .
 
-# Install everything from package.json
-npm install
-
-# Run library build
-npm run lib:build
+# Change directory to workspace root
+cd ../../ & ng build ngx-input-annotations
 
 # Unzip prepared testing files (app folder)
-unzip -o ./docs/app.zip -d ./../ngx-rapid-interface-builder-test/src
+unzip -o ./docs/app.zip -d ./../ngx-input-annotations-demo/src
+cp ./docs/index.html ../ngx-input-annotations-demo/src/
 
 # Open Visual Studio Code in workspace directory
 code ../../.
