94b4eb6fd53f1b1bdc38f363521676cb98eb96e6
crorisvanjski4 <crorisvanjski4@srce.hr>
2022-12-28 17:12:56 +0100

    Export InferredType

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import Error from "./src/components/Error";
 import Errors from "./src/components/Errors";
 import Form from "./src/components/Form";
+import InferredType from "./src/constants/InferredType";
 import Rule from "./src/decorators/validators/custom/Rule";
 import ValidatorService from "./src/service/ValidatorService";
 import { validators } from "./src/utils/ValidatorDecoratorUtils";
 
-export { ValidatorService, validators, Error, Errors, Form, Rule };
+export {
+	InferredType,
+	ValidatorService,
+	validators,
+	Error,
+	Errors,
+	Form,
+	Rule,
+};
