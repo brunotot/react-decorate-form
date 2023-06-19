30dc7f60c5297766faa6cd2ba6638ecf78e49670
Bruno Tot <brunotot10000@gmail.com>
2023-06-19 22:41:59 +0200

    build fixes

--- index.ts
+++ index.ts
@@ -1,25 +1,21 @@
-import Error from "./src/components/Error";
-import Errors from "./src/components/Errors";
 import Form, { FormContext } from "./src/components/Form";
 import useDecoratedValidation from "./src/hooks/useDecoratedValidation";
 import {
-	InferredType,
-	ValidatorService,
-	validators,
-	Rule,
-	ValidationResult,
+  InferredType,
+  ValidatorService,
+  validators,
+  Rule,
+  ValidationResult,
 } from "typescript-decorator-validation";
 
 export type { ValidationResult };
 
 export {
-	Form,
-	Error,
-	Errors,
-	InferredType,
-	ValidatorService,
-	validators,
-	Rule,
-	FormContext,
-	useDecoratedValidation,
+  Form,
+  InferredType,
+  ValidatorService,
+  validators,
+  Rule,
+  FormContext,
+  useDecoratedValidation,
 };
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,54 +1,54 @@
 import React, { createContext, useEffect, useMemo, useState } from "react";
 import {
-	Class,
-	ValidationHandler,
-	ErrorData,
+  Class,
+  ValidationHandler,
+  ErrorData,
 } from "typescript-decorator-validation";
 
 export type HTMLFormProps = React.DetailedHTMLProps<
-	React.FormHTMLAttributes<HTMLFormElement>,
-	HTMLFormElement
+  React.FormHTMLAttributes<HTMLFormElement>,
+  HTMLFormElement
 >;
 
 export type FormProps<T> = HTMLFormProps & {
-	handleSubmit: (data: T) => any;
-	model: Class<T>;
-	value: any;
+  handleSubmit: (data: T) => any;
+  model: Class<T>;
+  value: any;
 };
 
 export const FormContext = createContext<FormContextType<any>>(null as any);
 
 export type FormContextType<T> = {
-	state: T;
-	errors: ErrorData<T>;
-	validationHandler: ValidationHandler<T>;
+  state: T;
+  errors: ErrorData<T>;
+  validationHandler: ValidationHandler<T>;
 };
 
 export default function Form<T>(props: FormProps<T>) {
-	const { model, value, handleSubmit, ...args } = props;
-	const [errors, setErrors] = useState<ErrorData<T>>({});
-	const validationHandler = useMemo(() => new ValidationHandler(model), []);
+  const { model, value, handleSubmit, ...args } = props;
+  const [errors, setErrors] = useState<ErrorData<T>>({} as any);
+  const validationHandler = useMemo(() => new ValidationHandler(model), []);
 
-	const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		if (!validationHandler.hasErrors(value)) {
-			handleSubmit(validationHandler.buildInstance(value));
-		}
-	};
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!validationHandler.hasErrors(value)) {
+      handleSubmit(validationHandler.buildInstance(value));
+    }
+  };
 
-	useEffect(() => setErrors(validationHandler.getErrors(value)), [value]);
+  useEffect(() => setErrors(validationHandler.getErrors(value)), [value]);
 
-	const context = {
-		state: value,
-		validationHandler,
-		errors,
-	};
+  const context = {
+    state: value,
+    validationHandler,
+    errors,
+  };
 
-	return (
-		<FormContext.Provider value={context}>
-			<form {...args} onSubmit={onSubmit}>
-				{props.children}
-			</form>
-		</FormContext.Provider>
-	);
+  return (
+    <FormContext.Provider value={context}>
+      <form {...args} onSubmit={onSubmit}>
+        {props.children}
+      </form>
+    </FormContext.Provider>
+  );
 }
