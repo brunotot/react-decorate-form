89079111100b0de1d58a19fd686bbdf68eaa8ac3
Bruno Tot <brunotot10000@gmail.com>
2023-06-19 22:38:08 +0200

    update

--- src/hooks/useDecoratedValidation.ts
+++ src/hooks/useDecoratedValidation.ts
@@ -1,56 +1,56 @@
 import { useEffect, useMemo, useState } from "react";
 import {
-	Class,
-	ErrorData,
-	ValidationHandler,
+  Class,
+  ErrorData,
+  ValidationHandler,
+  ValidationClass,
 } from "typescript-decorator-validation";
 
 export type OnChangeHandlerType = (
-	event: React.ChangeEvent<HTMLInputElement>
+  event: React.ChangeEvent<HTMLInputElement>
 ) => void;
 
-export type DecoratedValidationType<K, T> = {
-	value: K;
-	setValue: React.Dispatch<React.SetStateAction<K>>;
-	isValid: boolean;
-	onChange: OnChangeHandlerType;
-	detailedErrors: ErrorData<T>;
-	errors: ErrorData<T>;
+export type DecoratedValidationType<T> = {
+  value: ValidationClass<T>;
+  setValue: React.Dispatch<React.SetStateAction<ValidationClass<T>>>;
+  isValid: boolean;
+  onChange: OnChangeHandlerType;
+  detailedErrors: ErrorData<T>;
+  errors: ErrorData<T>;
 };
 
 export type OnSubmitHandler<K> = (model: K) => void;
 
-export default function useDecoratedValidation<K, T>(
-	initialValue: K,
-	model: Class<T>
-): DecoratedValidationType<K, T> {
-	const [value, setValue] = useState<K>(initialValue);
-	const validationHandler = useMemo(
-		() => new ValidationHandler<T>(model as any),
-		[]
-	);
-	const [detailedErrors, setDetailedErrors] = useState<ErrorData<T>>({});
+export default function useDecoratedValidation<T>(
+  initialValue: ValidationClass<T>,
+  model: Class<T>
+): DecoratedValidationType<T> {
+  const [value, setValue] = useState<ValidationClass<T>>(initialValue);
+  const validationHandler = useMemo(
+    () => new ValidationHandler<T>(model as any),
+    []
+  );
+  const [detailedErrors, setDetailedErrors] = useState<ErrorData<T>>({} as any);
 
-	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setValue({
-			...value,
-			[event.target.name]: event.target.value,
-		});
-	};
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue({
+      ...value,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-	useEffect(
-		() => setDetailedErrors(validationHandler.getErrors(value as Object)),
-		[value]
-	);
+  useEffect(() => {
+    setDetailedErrors(validationHandler.getErrors(value as any));
+  }, [value]);
 
-	const isValid = !validationHandler.hasErrors(value as Object);
+  const isValid = !validationHandler.hasErrors(value as any);
 
-	return {
-		isValid,
-		value,
-		setValue,
-		onChange,
-		errors: detailedErrors,
-		detailedErrors,
-	};
+  return {
+    isValid,
+    value,
+    setValue,
+    onChange,
+    errors: detailedErrors,
+    detailedErrors,
+  };
 }
